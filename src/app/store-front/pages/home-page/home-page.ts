0d41b7d5d0e9e5adc696414330530115a8d8203d
Wilfredo Guerra <willigue@gmail.com>
2025-10-25 19:48:08 -0400

    listado de productos

--- src/app/store-front/pages/home-page/home-page.ts
+++ src/app/store-front/pages/home-page/home-page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ProductsService } from '@products/services/products.service';
 import { ProductCard } from '@store-front/components/product-card/product-card';
 
 @Component({
@@ -6,4 +8,15 @@ import { ProductCard } from '@store-front/components/product-card/product-card';
   imports: [ProductCard],
   templateUrl: './home-page.html',
 })
-export class HomePage { }
+export class HomePage {
+
+  productsService = inject(ProductsService);
+
+  productsResource = rxResource({
+    params: () => ({}),
+    stream: ({ params }) => {
+      return this.productsService.getProducts();
+    }
+  })
+
+}
