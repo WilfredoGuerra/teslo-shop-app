9edc645130cb3cbf1959d7a67e89cbfe056790fd
Wilfredo Guerra <willigue@gmail.com>
2025-10-25 19:31:19 -0400

    fuentes, tarjeta producto, path alias

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'montserrat': ['Montserrat', 'sans-serif']
+    },
+    extend: {
+      animation: {
+        fadeIn: 'fadeIn 0.3s ease-in-out'
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        }
+      }
+    },
   },
   plugins: [
     require('daisyui'),
